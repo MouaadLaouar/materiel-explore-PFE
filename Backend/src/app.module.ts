aa96b5f957f7f161d8c3c91129e10a11fc8733bd
MouaadLaouar <laouarmouaad@gmail.com>
2024-04-05 16:24:33 +0100

    do some Update

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MaterialModule } from './material/material.module';
+import { DepartementModule } from './departement/departement.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, MaterialModule, DepartementModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+const PORT = process.env.PORT || 5555;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    await app.listen(PORT, () => {
+      Logger.log(`Your NestJS app is live at http://localhost:${PORT} !`)
+    });
 }
 bootstrap();
