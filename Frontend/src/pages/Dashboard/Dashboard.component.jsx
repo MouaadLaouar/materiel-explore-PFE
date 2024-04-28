3e989dde24da6da772111ca7163a307299718cd4
Islem Dj <islemas23@gmail.com>
2024-04-28 21:26:14 +0100

    Add Statment For Users Not Logged in

--- Frontend/src/pages/Dashboard/Dashboard.component.jsx
+++ Frontend/src/pages/Dashboard/Dashboard.component.jsx
@@ -6,8 +6,10 @@ import User from "./User";
 import { useAtom } from "jotai";
 import { userIdAtom } from "../../atom";
 import SuperAdmin from "./SuperAdmin";
+import { useNavigate } from "react-router";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   const [error, setError] = useState(false);
   const [user, setUser] = useState(null);
   const [isLoggedIn] = useAtom(userIdAtom);
@@ -28,7 +30,26 @@ const Dashboard = () => {
   }, [isLoggedIn]);
 
   if (error) {
-    return <h1>Error</h1>;
+    return (
+      <>
+        <div className="w-11/12 m-auto text-center mt-20">
+          <h1 className="font-mdBold text-3xl mb-5">
+            Oops ! , You are Not Logged In
+          </h1>
+          <h1 className="font-mdBold text-2xl">
+            Try To Sign In to Your Account Or Create One{" "}
+            <a
+              className=" cursor-pointer text-teal-400"
+              onClick={() => {
+                navigate("/Signin");
+              }}
+            >
+              Here
+            </a>
+          </h1>
+        </div>
+      </>
+    );
   }
 
   if (user?.Role === "ADMIN") {
