6237ed38acddf57df12c85c9d6ee4755f2649403
Islem Dj <islemas23@gmail.com>
2024-05-24 05:02:51 +0100

    Some Updates

--- Frontend/src/Components/PopUp/PopUp.component.jsx
+++ Frontend/src/Components/PopUp/PopUp.component.jsx
@@ -29,7 +29,7 @@ export default function PasswordConfirmation({ children, open, setOpen }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:w-full sm:max-w-lg">
                 {children}
               </Dialog.Panel>
             </Transition.Child>
