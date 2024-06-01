ce10d770420e73594221bd770bf892de7de9388c
Islem Dj <islemas23@gmail.com>
2024-06-02 00:27:21 +0100

    .

--- Frontend/src/Components/NavBar/NavBar.Helper.js
+++ Frontend/src/Components/NavBar/NavBar.Helper.js
@@ -1,6 +1,5 @@
 export const Pages = [
   { name: "Home", href: "/" },
   { name: "Materials", href: "/materials" },
-  { name: "About Us", href: "/aboutUs" },
   { name: "Contact", href: "/contact" },
 ];
